a5195ae3694c09b0b549fd5a2f8bf67d081f34be
Hubert <142225610+hubertrokosz@users.noreply.github.com>
2024-02-26 22:43:08 +0100

    Added a toggle button the changes the read status on selected book

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const myLibrary = [
-    {title: "The Hunger Games" , author: "Suzanne Collins"},
-    {title: "Harry Potter", author: "J.K. Rowling"},
-    {title: "Pride and Prejudice", author: "Jane Austen"},
-    {title: "To Kill a Mockingbird", author: "Harper Lee"}
+    {title: "The Hunger Games", author: "Suzanne Collins", read: "yes"},
+    {title: "Harry Potter", author: "J.K. Rowling", read: "yes"},
+    {title: "Pride and Prejudice", author: "Jane Austen", read: "no"},
+    {title: "To Kill a Mockingbird", author: "Harper Lee", read: "yes"}
 ];
 
-function Book(title, author) {
+function Book(title, author, read) {
     this.title = title;
     this.author = author;
+    this.read = read;
 }
 
 document.getElementById("myForm").addEventListener("submit", function(event) {
@@ -16,10 +17,11 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
 
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
+    let read = document.querySelector("input[name='agreement']:checked").id;
 
     let book;
 
-    book = new Book(title, author);
+    book = new Book(title, author, read);
     myLibrary.push(book);
 
     console.log(myLibrary);
@@ -40,23 +42,43 @@ function displayLibrary() {
         let card = document.createElement("div");
         card.classList.add("card");
         card.textContent = myLibrary[index].title + "\n" + myLibrary[index].author;
+
+        if (myLibrary[index].read == "yes") card.textContent += "\n Read";
+        else if (myLibrary[index].read == "no") card.textContent += "\n Not read yet";
+
         card.setAttribute("data-index", index);
 
         library.appendChild(card); 
 
-        let button = document.createElement("button");
-        button.textContent = "X";
-        button.classList.add("button");
+        let buttonDelete = document.createElement("button");
+        buttonDelete.textContent = "X";
+        buttonDelete.classList.add("button-delete");
 
-        card.appendChild(button);
+        card.appendChild(buttonDelete);
+
+        let buttonToggle = document.createElement("button");
+        buttonToggle.textContent = "Toggle";
+        buttonToggle.classList.add("button-toggle");
+
+        card.appendChild(buttonToggle);
 
     }
 
-    document.querySelectorAll(".button").forEach(function(button){
+    document.querySelectorAll(".button-delete").forEach(function(button){
         button.addEventListener("click", function() {
             let parent = this.parentNode.getAttribute("data-index");
             console.log(parent);
             myLibrary.splice(parent, 1);
+            displayLibrary();
+        })
+    });
+
+    document.querySelectorAll(".button-toggle").forEach(function(button){
+        button.addEventListener("click", function() {
+            let parent = this.parentNode.getAttribute("data-index");
+            
+            if (myLibrary[parent].read == "no") myLibrary[parent].read = "yes";
+            else if (myLibrary[parent].read == "yes") myLibrary[parent].read = "no";
             displayLibrary();
         })
     });
